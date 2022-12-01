0211e458af32172b77a2949854fc72e65a161053
Michael Erne <michael.erne@gmail.com>
2022-12-01 06:11:33 +0100

    day01.py

--- day01.py
+++ day01.py
@@ -2,18 +2,37 @@ from run_util import run_puzzle
 
 
 def part_a(data):
-    input = [int(x) for x in data.split(',')]
-    return 0
+    elfes = [x for x in data.split('\n\n')]
+    calories = []
+    for elf in elfes:
+        calories.append(sum([int(x) for x in elf.split('\n')]))
+    return max(calories)
 
 
 def part_b(data):
-    input = [int(x) for x in data.split(',')]
-    return 0
+    elfes = [x for x in data.split('\n\n')]
+    calories = []
+    for elf in elfes:
+        calories.append(sum([int(x) for x in elf.split('\n')]))
+    return sum(sorted(calories, reverse=True)[0:3])
 
 
 def main():
     examples = [
-        ("""""", 1, 1)
+        ("""1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000""", 24000, 45000)
     ]
     day = int(__file__.split('/')[-1].split('.')[0][-2:])
     run_puzzle(day, part_a, part_b, examples)
