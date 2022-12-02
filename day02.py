059cad2887c6e3aa4e4bb95e4ebc037df325ba94
Michael Erne <michael.erne@gmail.com>
2022-12-02 06:42:05 +0100

    rework day02

--- day02.py
+++ day02.py
@@ -4,46 +4,24 @@ from run_util import run_puzzle
 def part_a(data):
     games = [line.split(' ') for line in data.split('\n')]
 
-    result = {("A", "X"): 1 + 3, ("A", "Y"): 2 + 6, ("A", "Z"): 3 + 0,
-              ("B", "X"): 1 + 0, ("B", "Y"): 2 + 3, ("B", "Z"): 3 + 6,
-              ("C", "X"): 1 + 6, ("C", "Y"): 2 + 0, ("C", "Z"): 3 + 3}
+    move_score = {'X': 1, 'Y': 2, 'Z': 3}
+    result_score = {('A', 'X'): 3, ('A', 'Y'): 6, ('A', 'Z'): 0,
+                    ('B', 'X'): 0, ('B', 'Y'): 3, ('B', 'Z'): 6,
+                    ('C', 'X'): 6, ('C', 'Y'): 0, ('C', 'Z'): 3}
 
-    score = sum([result[tuple(game)] for game in games])
+    score = sum([move_score[my_move] + result_score[(op_move, my_move)] for op_move, my_move in games])
     return score
 
 
 def part_b(data):
     games = [line.split(' ') for line in data.split('\n')]
 
-    goal_lose = {"B": "X", "C": "Y", "A": "Z"}
-    goal_win = {"A": "Y", "B": "Z", "C": "X"}
-    goal_draw = {"A": "X", "B": "Y", "C": "Z"}
+    move_score = {'X': 0, 'Y': 3, 'Z': 6}
+    result_score = {('A', 'X'): 3, ('A', 'Y'): 1, ('A', 'Z'): 2,
+                    ('B', 'X'): 1, ('B', 'Y'): 2, ('B', 'Z'): 3,
+                    ('C', 'X'): 2, ('C', 'Y'): 3, ('C', 'Z'): 1}
 
-    def get_result(op_move, my_goal):
-        game_score = 0
-
-        match my_goal:
-            case "X":
-                my_move = goal_lose[op_move]
-                game_score += 0
-            case "Y":
-                my_move = goal_draw[op_move]
-                game_score += 3
-            case _:  # case "Z":
-                my_move = goal_win[op_move]
-                game_score += 6
-
-        match my_move:
-            case "X":
-                game_score += 1
-            case "Y":
-                game_score += 2
-            case "Z":
-                game_score += 3
-
-        return game_score
-
-    score = sum([get_result(*game) for game in games])
+    score = sum([move_score[my_move] + result_score[(op_move, my_move)] for op_move, my_move in games])
     return score
 
 
